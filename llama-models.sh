f03a7600ad7ef2773ccc8df6aa12d933f8005b9a
m-weida <117124336+m-weida@users.noreply.github.com>
2026-07-16 10:39:15 +0200

    Drop redundant $ from array index assignments in check-updates
    Fixes shellcheck SC2004 ($/${} is unnecessary on arithmetic variables)
    in cmd_check_updates: the array element assignment targets used
    $repo_index where the subscript is already arithmetic context.
    Expansion-side ${arr[$repo_index]} subscripts are left unchanged to
    stay consistent with the rest of the file.

--- llama-models.sh
+++ llama-models.sh
@@ -1143,8 +1143,8 @@ EOF
       repo_current_commits+=("$commit")
       repo_file_lists+=("$path")
     else
-      repo_current_commits[$repo_index]="$commit"
-      repo_file_lists[$repo_index]="${repo_file_lists[$repo_index]}"$'\n'"$path"
+      repo_current_commits[repo_index]="$commit"
+      repo_file_lists[repo_index]="${repo_file_lists[$repo_index]}"$'\n'"$path"
     fi
   done
 
